c0b704ded9ca1467a21e61d6148ac25bb1ae1c4f
OrayKT <oray.kt@gmail.com>
2019-11-01 20:25:36 +0100

    config

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,9 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  mongoURI: process.env.mongoURI,
+  cookieKey: process.env.cookieKey,
+  userName: process.env.userName,
+  userEmail: process.env.userEmail,
+  albumTitle: process.env.albumTitle,
+  albumPerformer: process.env.albumPerformer,
+  albumCost: process.env.albumCost
 }
